b2d816a3f00da53b43154c477c7d1f0906ca888b
georgi-dell <georgichichev15@gmail.com>
2023-01-16 01:22:55 +0100

    Add icon to table

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,4 +1,5 @@
 import { Table } from "@mantine/core";
+import { IconArrowDownCircle, IconArrowUpCircle } from "@tabler/icons";
 import React from "react";
 
 export function DataTable({ dayData }) {
@@ -8,16 +9,28 @@ export function DataTable({ dayData }) {
         <Table>
             <thead>
                 <tr>
-                    <th>Type</th>
-                    <th>Amount</th>
+                    <th style={{ textAlign: "center" }}>Expense</th>
+                    <th style={{ textAlign: "center" }}>Amount</th>
+                    <th style={{ textAlign: "center" }}>Movement</th>
                 </tr>
             </thead>
             <tbody>
                 {dayKeys.map((key) => {
                     return (
                         <tr key={key}>
-                            <td>{dayData[key].expenseType}</td>
-                            <td>{dayData[key].amount}</td>
+                            <td style={{ textAlign: "center" }}>
+                                {dayData[key].expenseType}
+                            </td>
+                            <td
+                                style={{ textAlign: "center" }}
+                            >{`${dayData[key].amount} лв`}</td>
+                            <td style={{ textAlign: "center" }}>
+                                {dayData[key].movementType === "income" ? (
+                                    <IconArrowUpCircle color="green" />
+                                ) : (
+                                    <IconArrowDownCircle color="red" />
+                                )}
+                            </td>
                         </tr>
                     );
                 })}
