11983000f3979ede2f498f8d44a4b2a8f046f586
georgi-dell <georgichichev15@gmail.com>
2023-01-21 23:52:51 +0100

    Create td and th coponents

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,49 +1,49 @@
 import { Table } from "@mantine/core";
-import {
-    IconArrowDownCircle,
-    IconArrowUpCircle,
-    // IconTrash,
-} from "@tabler/icons";
-import React from "react";
+import { IconArrowDownCircle, IconArrowUpCircle } from "@tabler/icons";
+import React, { PropsWithChildren } from "react";
+
+const Th = ({ children }: PropsWithChildren) => {
+  return <th style={{ textAlign: "center" }}>{children}</th>;
+};
+
+const Td = ({ children }: PropsWithChildren) => {
+  return <td style={{ textAlign: "center" }}>{children}</td>;
+};
 
 export function DataTable({ dayData }) {
-    const dayKeys = Object.keys(dayData);
+  const dayKeys = Object.keys(dayData);
 
-    return (
-        <Table>
-            <thead>
-                <tr>
-                    <th style={{ textAlign: "center" }}>Expense</th>
-                    <th style={{ textAlign: "center" }}>Amount</th>
-                    <th style={{ textAlign: "center" }}>Movement</th>
-                </tr>
-            </thead>
-            <tbody>
-                {dayKeys.map((key) => {
-                    return (
-                        <tr key={key}>
-                            <td style={{ textAlign: "center" }}>
-                                {dayData[key].expenseType}
-                            </td>
-                            <td
-                                style={{ textAlign: "center" }}
-                            >{`${dayData[key].amount} лв`}</td>
-                            <td style={{ textAlign: "center" }}>
-                                {dayData[key].movementType === "income" ? (
-                                    <IconArrowUpCircle color="green" />
-                                ) : (
-                                    <IconArrowDownCircle color="red" />
-                                )}
-                            </td>
-                            {/* <td style={{ textAlign: "center" }}>
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <Th>Expense</Th>
+          <Th>Amount</Th>
+          <Th>Movement</Th>
+        </tr>
+      </thead>
+      <tbody>
+        {dayKeys.map((key) => {
+          return (
+            <tr key={key}>
+              <Td>{dayData[key].expenseType}</Td>
+              <Td>{`${dayData[key].amount} лв`}</Td>
+              <Td>
+                {dayData[key].movementType === "income" ? (
+                  <IconArrowUpCircle color="green" />
+                ) : (
+                  <IconArrowDownCircle color="red" />
+                )}
+              </Td>
+              {/* <td style={{ textAlign: "center" }}>
                                 <ActionIcon style={{ alignSelf: "center" }}>
                                     <IconTrash />
                                 </ActionIcon>
                             </td> */}
-                        </tr>
-                    );
-                })}
-            </tbody>
-        </Table>
-    );
+            </tr>
+          );
+        })}
+      </tbody>
+    </Table>
+  );
 }
